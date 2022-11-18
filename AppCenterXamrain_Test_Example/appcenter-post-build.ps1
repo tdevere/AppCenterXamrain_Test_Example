74a98f426c7fc0dcde8de432e251811ec54d73b2
tdevere <tdevere@microsoft.com>
2022-11-18 11:14:44 -0600

    $projectFile

--- AppCenterXamrain_Test_Example/appcenter-post-build.ps1
+++ AppCenterXamrain_Test_Example/appcenter-post-build.ps1
@@ -1,8 +1,8 @@
     ##Fix Specific Version of Xamarin.UITest
-    $projectFile = '/Users/runner/work/1/a/GeneratedTest/AppCenter.UITest.Android/AppCenter.UITest.Android.csproj'
+    $projectFile =  '/Users/runner/work/1/a/GeneratedTest/AppCenter.UITest.Android/AppCenter.UITest.Android.csproj'
     $packagesFile = '/Users/runner/work/1/a/GeneratedTest/AppCenter.UITest.Android/packages.config'
 
-    (Get-Content $projectFile).Replace("Xamarin.UITest.4.0.0", "Xamarin.UITest.3.2.9") | Set-Content  $projectFilej
+    (Get-Content $projectFile).Replace("Xamarin.UITest.4.0.0", "Xamarin.UITest.3.2.9") | Set-Content  $projectFile
     Get-Content $projectFile
     (Get-Content $packagesFile).Replace('4.0.0','3.2.9') | Set-Content  $packagesFile
     Get-Content $packagesFile
