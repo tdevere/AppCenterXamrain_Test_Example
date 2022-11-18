e5d8d1ddf936e535c939e163e495ef9a1cf5e6ea
tdevere <tdevere@microsoft.com>
2022-11-17 20:48:25 -0600

    printing out changes

--- AppCenterXamrain_Test_Example/appcenter-post-build.ps1
+++ AppCenterXamrain_Test_Example/appcenter-post-build.ps1
@@ -18,7 +18,9 @@ else
     $packagesFile = '/Users/runner/work/1/a/GeneratedTest/AppCenter.UITest.Android/packages.config'
 
     (Get-Content $projectFile).Replace("Xamarin.UITest.4.0.0", "Xamarin.UITest.3.2.9") | Set-Content  $projectFilej
+    Get-Content $projectFile
     (Get-Content $packagesFile).Replace('4.0.0','3.2.9') | Set-Content  $packagesFile
+    Get-Content $packagesFile
 
     nuget restore -NonInteractive /Users/runner/work/1/a/GeneratedTest/AppCenter.UITest.Android.sln
 
