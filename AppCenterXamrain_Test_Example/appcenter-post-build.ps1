777bfc1a7acf0cc23ebc88b28181255518a11f46
tdevere <tdevere@microsoft.com>
2022-11-18 11:33:31 -0600

    only modifing $projectFile

--- AppCenterXamrain_Test_Example/appcenter-post-build.ps1
+++ AppCenterXamrain_Test_Example/appcenter-post-build.ps1
@@ -3,14 +3,14 @@
     Write-Host "Project File Before Change"
     (Get-Content $projectFile)
     
-    $packagesFile = '/Users/runner/work/1/a/GeneratedTest/AppCenter.UITest.Android/packages.config'
-    Write-Host "Package File Before Change"
-    (Get-Content $packagesFile)
+    #$packagesFile = '/Users/runner/work/1/a/GeneratedTest/AppCenter.UITest.Android/packages.config'
+    #Write-Host "Package File Before Change"
+    #(Get-Content $packagesFile)
 
     (Get-Content $projectFile).Replace("Xamarin.UITest.4.0.0", "Xamarin.UITest.3.2.9") | Set-Content $projectFile
     Write-Host "Project File After Change"
     Get-Content $projectFile
     
-    (Get-Content $packagesFile).Replace('4.0.0','3.2.9') | Set-Content $packagesFile
-    Write-Host "Package File After Change"
-    Get-Content $packagesFile
+    #(Get-Content $packagesFile).Replace('4.0.0','3.2.9') | Set-Content $packagesFile
+    #Write-Host "Package File After Change"
+    #Get-Content $packagesFile
