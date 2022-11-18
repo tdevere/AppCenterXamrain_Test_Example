c64f2c269b4db681815855f375452bc6cc0a2ee5
tdevere <tdevere@microsoft.com>
2022-11-18 11:22:27 -0600

    adding some output to troubleshoot script failure

--- AppCenterXamrain_Test_Example/appcenter-post-build.ps1
+++ AppCenterXamrain_Test_Example/appcenter-post-build.ps1
@@ -1,8 +1,16 @@
     ##Fix Specific Version of Xamarin.UITest
     $projectFile =  '/Users/runner/work/1/a/GeneratedTest/AppCenter.UITest.Android/AppCenter.UITest.Android.csproj'
+    Write-Host "Project File Before Change"
+    (Get-Content $projectFile)
+    
     $packagesFile = '/Users/runner/work/1/a/GeneratedTest/AppCenter.UITest.Android/packages.config'
+    Write-Host "Package File Before Change"
+    (Get-Content $packagesFile)
 
-    (Get-Content $projectFile).Replace("Xamarin.UITest.4.0.0", "Xamarin.UITest.3.2.9") | Set-Content  $projectFile
+    (Get-Content $projectFile).Replace("Xamarin.UITest.4.0.0", "Xamarin.UITest.3.2.9") | Set-Content $projectFile
+    Write-Host "Project File After Change"
     Get-Content $projectFile
-    (Get-Content $packagesFile).Replace('4.0.0','3.2.9') | Set-Content  $packagesFile
+    
+    (Get-Content $packagesFile).Replace('4.0.0','3.2.9') | Set-Content $packagesFile
+    Write-Host "Package File After Change"
     Get-Content $packagesFile
