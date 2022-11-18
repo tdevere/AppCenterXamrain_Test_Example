558fbc3fff7e67a1e3c742b7a828ac5f72f84dcc
tdevere <tdevere@microsoft.com>
2022-11-17 21:24:18 -0600

    removed "."

--- AppCenterXamrain_Test_Example/appcenter-post-build.sh
+++ AppCenterXamrain_Test_Example/appcenter-post-build.sh
@@ -2,4 +2,4 @@
 chmod +x /Users/runner/work/1/s/AppCenterXamrain_Test_Example/appcenter-post-build.ps1
 
 echo "Moving On"
-powershell ./Users/runner/work/1/s/AppCenterXamrain_Test_Example/appcenter-post-build.ps1
+powershell /Users/runner/work/1/s/AppCenterXamrain_Test_Example/appcenter-post-build.ps1
