7e445e9b7df933588291a0c59b154a90489bfda9
tdevere <tdevere@microsoft.com>
2022-11-17 21:20:52 -0600

    added powershell interface

--- AppCenterXamrain_Test_Example/appcenter-post-build.sh
+++ AppCenterXamrain_Test_Example/appcenter-post-build.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 chmod +x /Users/runner/work/1/s/AppCenterXamrain_Test_Example/appcenter-post-build.ps1
 
-getfacl /Users/runner/work/1/s/AppCenterXamrain_Test_Example/appcenter-post-build.ps1
-
-
 echo "Moving On"
-/Users/runner/work/1/s/AppCenterXamrain_Test_Example/appcenter-post-build.ps1
+powershell ./Users/runner/work/1/s/AppCenterXamrain_Test_Example/appcenter-post-build.ps1
