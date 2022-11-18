114dec47864d961e64ab5d2bfa9296a42384d4c0
tdevere <tdevere@microsoft.com>
2022-11-18 11:00:46 -0600

    trying to clean this up

--- AppCenterXamrain_Test_Example/appcenter-post-build.ps1
+++ AppCenterXamrain_Test_Example/appcenter-post-build.ps1
@@ -1,18 +1,3 @@
-Write-Host "The Post Build Script START"
-Write-Host "AppCenterXamrain_Test_Example"
-
-if (!$env:AppCenterTokenForTest)
-{
-    Write-Host "This script only runs within the context of App Center builds"
-    exit
-}
-else
-{
-
-    appcenter login --token $AppCenterTokenForTest
-
-    appcenter test generate uitest --platform android --output-path /Users/runner/work/1/a/GeneratedTest
-
     ##Fix Specific Version of Xamarin.UITest
     $projectFile = '/Users/runner/work/1/a/GeneratedTest/AppCenter.UITest.Android/AppCenter.UITest.Android.csproj'
     $packagesFile = '/Users/runner/work/1/a/GeneratedTest/AppCenter.UITest.Android/packages.config'
@@ -21,19 +6,3 @@ else
     Get-Content $projectFile
     (Get-Content $packagesFile).Replace('4.0.0','3.2.9') | Set-Content  $packagesFile
     Get-Content $packagesFile
-
-    nuget restore -NonInteractive /Users/runner/work/1/a/GeneratedTest/AppCenter.UITest.Android.sln
-
-    xbuild /Users/runner/work/1/a/GeneratedTest/AppCenter.UITest.Android.sln /p:Configuration=Release
-
-    appcenter test prepare uitest --artifacts-dir /Users/runner/work/1/a/Artifacts --app-path /Users/runner/work/1/a/build/com.ManualTestOnDevice.ReleaseTest1.apk --build-dir /Users/runner/work/1/a/GeneratedTest/AppCenter.UITest.Android/bin/Release --debug --quiet
-
-    #Debug Line
-    Write-Host "cat /Users/runner/work/1/a/Artifacts/manifest.json"
-    Get-Content '/Users/runner/work/1/a/Artifacts/manifest.json'
-
-    appcenter test run manifest --manifest-path /Users/runner/work/1/a/Artifacts/manifest.json --app-path /Users/runner/work/1/a/build/com.ManualTestOnDevice.yourapp.apk --app AppCenterSupportDocs/ManualTestOnDevice --devices any_top_1_device --test-series launch-tests --locale en_US -p msft/test-run-origin=Build/Launch --debug --quiet --token $AppCenterTokenForTest
-
-}
-
-Write-Host  "The Post Build Script END"
--- AppCenterXamrain_Test_Example/appcenter-post-build.sh
+++ AppCenterXamrain_Test_Example/appcenter-post-build.sh
@@ -1,5 +1,32 @@
 #!/usr/bin/env bash
 chmod +x /Users/runner/work/1/s/AppCenterXamrain_Test_Example/appcenter-post-build.ps1
 
-echo "Moving On"
+echo "The Post Build Script START"
+
+if [ -z "$AppCenterTokenForTest" ]
+then 
+    echo "This script only runs within the context of App Center builds"
+    exit
+fi
+
+appcenter login --token $AppCenterTokenForTest
+
+appcenter test generate uitest --platform android --output-path /Users/runner/work/1/a/GeneratedTest
+
+echo "PowerShell Start"
 powershell /Users/runner/work/1/s/AppCenterXamrain_Test_Example/appcenter-post-build.ps1
+echo "PowerShell End"
+nuget restore -NonInteractive /Users/runner/work/1/a/GeneratedTest/AppCenter.UITest.Android.sln
+
+xbuild /Users/runner/work/1/a/GeneratedTest/AppCenter.UITest.Android.sln /p:Configuration=Release
+
+appcenter test prepare uitest --artifacts-dir /Users/runner/work/1/a/Artifacts --app-path /Users/runner/work/1/s/AppCenterXamrain_Test_Example/bin/Debug/com.companyname.appcenterxamrain_test_example.apk --build-dir /Users/runner/work/1/a/GeneratedTest/AppCenter.UITest.Android/bin/Release --debug --quiet
+
+#Debug Line
+echo "cat /Users/runner/work/1/a/Artifacts/manifest.json"
+cat /Users/runner/work/1/a/Artifacts/manifest.json
+
+appcenter test run manifest --manifest-path /Users/runner/work/1/a/Artifacts/manifest.json --app-path /Users/runner/work/1/s/AppCenterXamrain_Test_Example/bin/Debug/com.companyname.appcenterxamrain_test_example.apk --app Examples/Android_Xamarin_UI_Test --devices "Examples/tdevere-device-set-1" --test-series launch-tests --locale en_US -p msft/test-run-origin=Build/Launch --debug --quiet --test-chunk --token $AppCenterTokenForTest
+
+echo "The Post Build Script END"
+
