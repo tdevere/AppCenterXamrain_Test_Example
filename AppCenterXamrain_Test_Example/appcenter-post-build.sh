4266df341d731889bcb83baa0faaade3aea760d3
tdevere <tdevere@microsoft.com>
2022-11-17 21:15:56 -0600

    permission denied error on App Center

--- AppCenterXamrain_Test_Example/appcenter-post-build.sh
+++ AppCenterXamrain_Test_Example/appcenter-post-build.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
+chmod +x /Users/runner/work/1/s/AppCenterXamrain_Test_Example/appcenter-post-build.ps1
+
+getfacl /Users/runner/work/1/s/AppCenterXamrain_Test_Example/appcenter-post-build.ps1
+
+
+echo "Moving On"
 /Users/runner/work/1/s/AppCenterXamrain_Test_Example/appcenter-post-build.ps1
